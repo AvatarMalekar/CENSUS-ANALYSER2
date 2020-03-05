01e8c1fafb75f17c1b3f12975de7af701efdafbd
Your Name <avatarmalekar4@gmail.com>
2020-03-05 14:27:01 +0530

    REFACTORED:created loader class to load census data

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -22,35 +22,10 @@ public class CensusAnalyser<E> {
 
     public int loadIndiaCensusData(String csvFilePath){
 
-        return loadCensusData(csvFilePath,IndiaCensusCSV.class);
+        censusMap=new CensusLoader().loadCensusData(csvFilePath,IndiaCensusCSV.class);
+        censusList=censusMap.values().stream().collect(Collectors.toList());
+        return censusMap.size();
     }
-
-    private <E>int loadCensusData(String csvFilePath,Class<E> className) {
-        CsvToBean<E> csvToBean;
-        try(Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
-            ICSVBuilder icsvBuilder= CSVBuilderFactory.createBuilder();
-            Iterator<E> stateCensus=icsvBuilder.getCSVIterator(reader,className);
-            Iterable<E> stateCodeCSV= ()-> stateCensus;
-            if(className.getName().equals("censusanalyser.IndiaCensusCSV")){
-                StreamSupport.stream(stateCodeCSV.spliterator(),false)
-                    .map(IndiaCensusCSV.class::cast)
-                    .forEach(code->censusMap.put(code.state,new CensusDTO(code)));}
-            if(className.getName().equals("censusanalyser.USCensusCSV")){
-                StreamSupport.stream(stateCodeCSV.spliterator(),false)
-                        .map(USCensusCSV.class::cast)
-                        .forEach(code->censusMap.put(code.stateUs,new CensusDTO(code)));}
-            censusList=censusMap.values().stream().collect(Collectors.toList());
-            return censusList.size();
-        } catch (IOException e) {
-            throw new CensusAnalyserException(e.getMessage(),
-                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
-        }
-        catch (IllegalStateException e) {
-            throw new CensusAnalyserException(e.getMessage(),
-                    CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
-        }
-    }
-
 
     public int loadIndiaStateData(String indiaStateCode) {
         CsvToBean<IndiaStateCodeCSV> csvToBean;
@@ -123,7 +98,9 @@ public class CensusAnalyser<E> {
         return json;
     }
     public int loadUSCensusData(String usCensusCsvFilePath) {
-        return loadCensusData(usCensusCsvFilePath,USCensusCSV.class);
+        censusMap=new CensusLoader().loadCensusData(usCensusCsvFilePath,USCensusCSV.class);
+        censusList=censusMap.values().stream().collect(Collectors.toList());
+        return censusMap.size();
     }
 }
 
